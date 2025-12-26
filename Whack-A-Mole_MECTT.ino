35bf189b3f12af2f2cf8c61492f1a928d4f42196
Anushka768 <anushka2764@gmail.com>
2025-12-26 14:13:38 -0500

    with time bar and fixed logic

--- Whack-A-Mole_MECTT.ino
+++ Whack-A-Mole_MECTT.ino
@@ -1,4 +1,5 @@
-// Whack-A-Mole Inside Out Edition: McMaster Engineering Concrete Toboggan Team 
+// Whack-A-Mole: Inside Out Edition
+// McMaster Engineering Concrete Toboggan Team 
 
 // some knowledgable info
 // pinMode + digitalWrite control electricity, LEDC controls waveforms, and MD_Parola controls devices.
@@ -20,6 +21,7 @@ struct Mole {
   int ledPin;
   int buttonPin;
   bool active;
+  bool wasHit;
 };
 
 const int NUM_MOLES = 5;
@@ -29,8 +31,8 @@ const int BUZZER_PIN = 4;
 
 // Game Timing
 const unsigned long GAME_DURATION = 60000; // unsigned long used because time can never be negative
-const unsigned long START_INTERVAL = 1000;
-const unsigned long MIN_INTERVAL = 200;
+const unsigned long START_INTERVAL = 1200;
+const unsigned long MIN_INTERVAL = 300;
 
 // Matrix Setup ---------------------------------------------------------
 
@@ -40,8 +42,9 @@ const unsigned long MIN_INTERVAL = 200;
 # define DATA_PIN 21
 # define CLK_PIN 22
 
-// creates an object named matrix of type MD_Parola
+// creates an object named matrix
 MD_Parola matrix(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
 // initial game setup ---------------------------------------------------------------
 
@@ -49,15 +52,15 @@ GameState gameState = IDLE;
 Difficulty difficulty = MEDIUM;
 
 Mole moles[NUM_MOLES] = {
-  {25, 14, false},
-  {26, 27, false},
-  {32, 12, false},
-  {33, 13, false},
-  {23, 18, false}
+  {25, 14, false, false},
+  {26, 27, false, false},
+  {32, 12, false, false},
+  {33, 13, false, false},
+  {23, 18, false, false}
 };
 
 int score = 0;
-int activeMole = -1; // -1 means no mole is on
+//int activeMole = -1; // -1 means no mole is on
 
 // store time stamps instead of delays as delay freezes the game
 unsigned long gameStartTime;
@@ -68,6 +71,9 @@ unsigned long moleInterval = START_INTERVAL;
 unsigned long countdownTimer;
 int countdownValue = 3;
 
+// Button Edge Tracking -------------------------------------------------------
+bool lastResetBtn = HIGH;
+bool lastDiffBtn = HIGH;
 // set up Passive Buzzer --------------------------------------------------------------
 
 const int BUZZER_CH = 0;
@@ -95,7 +101,7 @@ void showText (const char* msg) { // meaning of having pointer here : msg points
   matrix.displayText(
     msg, // points to character array , passed by reference
     PA_CENTER, // centre the text
-    80, // used for scrolling text - speed parameter
+    0, // used for scrolling text - speed parameter
     0, // pause parameter for animation
     PA_PRINT, // effect in - alternatives: PA_SCROLL_LEFT, PA_SCROLL_RIGHT, PA_FADE
     PA_NO_EFFECT); // effect out 
@@ -114,17 +120,28 @@ void showCountdown( int n ) {
   showText(buf);
 }
 
+void showTimeBar () {
+  unsigned long elapsed = millis() - gameStartTime;
+  if (elapsed > GAME_DURATION) elapsed = GAME_DURATION;
 
+  int litCols = map(GAME_DURATION - elapsed, 0, GAME_DURATION, 0, 32); // 4 modules, 8 columns each -- 4 × 8 = 32 columns
+
+  for (int col = 0; col < 32; col++) {
+    mx.setPoint(7, col, col < litCols);
+  }
+}
 // Game Helper Functions------------------------------------------------------------
 
 void clearMoles() {
   for(int i = 0; i < NUM_MOLES; i++) {
     moles[i].active = false;
+    moles[i].wasHit = false;
     digitalWrite(moles[i].ledPin, LOW);
   }
 }
 
 void spawnMole() {
+    
   clearMoles();
 
   int count = (difficulty == HARD) ? random(2, 4) : 1;
@@ -137,33 +154,46 @@ void spawnMole() {
     } while (moles[index].active);
 
     moles[index].active = true;
+    moles[index].wasHit = false;
     digitalWrite(moles[index].ledPin, HIGH); // digitalWrite to set pin state
   }
 
   lastMoleTime = millis();
 }
 
+void evaluateMoles() {
+  int hits = 0;
+  int misses = 0;
+
+  for(int i = 0; i < NUM_MOLES; i++) {
+    if( moles[i].active) {
+      if(moles[i].wasHit) hits++;
+      else misses++;
+    }
+  }
+
+  score += hits;
+  score = max(0, score - misses);
+
+  if (hits > 0) buzzerHappy();
+  else if (misses > 0) buzzerSad();
+
+  moleInterval = max(MIN_INTERVAL, START_INTERVAL - score * 10); // max: prevents impossible speed, score*10: faster per point, START_INTERVAL: initial slow speed
+}
+
 void resetGame() {
   score = 0;
   moleInterval = START_INTERVAL;
-  gameState = COUNTDOWN;
   countdownValue = 3;
   countdownTimer = millis();
   clearMoles();
+  gameState = COUNTDOWN;
 }
 
 void endGame() {
-  gameState = GAME_OVER;
   clearMoles();
-
-  if (score >= 50) {
-    showText("WIN");
-    buzzerHappy();;
-  }
-  else {
-    showText("LOST");
-    buzzerSad();
-  }
+  gameState = GAME_OVER;
+  showText(score >= 50 ? "WIN" : "LOST");
 }
 
 void setup() {
@@ -179,7 +209,8 @@ void setup() {
 
   ledcAttach(BUZZER_PIN, 2000, 8); // 2000 - frequency(Hz), 8 - Resolution(bits),  we use ledc for buzzer as it takes square wave form
 
-  matrix.begin(); // makes the display library ready
+  mx.begin(); // for MD_MAX72XX
+  matrix.begin(); // makes the display library ready for MD Parola
   matrix.setIntensity(5); // brightness
   matrix.displayClear();
 
@@ -188,20 +219,28 @@ void setup() {
 }
 
 void loop() {
-  
-  // toggle difficulty
-  if(digitalRead(DIFF_BTN_PIN) == LOW) {
+    
+  bool resetBtn = digitalRead(RESET_BTN_PIN);
+  bool diffBtn  = digitalRead(DIFF_BTN_PIN);
+
+  // Reset Btn
+  if (lastResetBtn == HIGH && resetBtn == LOW) {
+    if (gameState == IDLE || gameState == GAME_OVER) {
+      resetGame();
+    }
+    delay(200);
+  }
+  lastResetBtn = resetBtn;
+
+  // Difficulty Btn
+  if ((gameState == IDLE || gameState == COUNTDOWN || gameState == GAME_OVER) && lastDiffBtn == HIGH && diffBtn == LOW) {
     difficulty = (difficulty == MEDIUM) ? HARD : MEDIUM;
     showText(difficulty == HARD ? "HARD" : "MED");
-    delay(500);
-  }
-
-  // Start or Reset
-  if (digitalRead(RESET_BTN_PIN) == LOW && (gameState == IDLE || gameState == GAME_OVER)) {
-    resetGame();
     delay(300);
   }
+  lastDiffBtn = diffBtn;
 
+  // Countdown
   if (gameState == COUNTDOWN) {
     if(millis() - countdownTimer >= 1000) { // asks if 1 second has passed since the last countdown update
       countdownTimer = millis(); // reset timer
@@ -218,36 +257,28 @@ void loop() {
   }
 
   if (gameState == PLAYING) {
+    showTimeBar ();
     if(millis() - gameStartTime >= GAME_DURATION) {
       endGame();
       return;
     }
 
     if(millis() - lastMoleTime >= moleInterval) {
+      evaluateMoles();
       spawnMole();
     }
 
     showScore();
-
+    
     for(int i = 0; i < NUM_MOLES; i++) {
       if(digitalRead(moles[i].buttonPin) == LOW) {
 
-        if(moles[i].active) {
-          score++;
-          buzzerHappy();
-          moleInterval = max(MIN_INTERVAL, START_INTERVAL - score * 10); // max: prevents impossible speed, score*10: faster per point, START_INTERVAL: initial slow speed
+        if(moles[i].active && !moles[i].wasHit) {
+          moles[i].wasHit = true;
+          digitalWrite(moles[i].ledPin, LOW);
         }
-        else{
-          score--;
-          buzzerSad();
-        }
-
-        clearMoles();
-        delay(150);
+        delay(120);
       }
     }
-  }
-  if (gameState == IDLE) {
-    showText("MECTT");
   }
 }
